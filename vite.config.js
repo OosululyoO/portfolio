7ae0faa58f85a451d7d11b5a81fdb67cd3c7b05d
OosululyoO <e940416@gmail.com>
2026-05-12 16:35:37 +0800

    ci: add deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  // 這裡非常重要！必須對應你的 GitHub Repo 名稱
+  base: '/portfolio/', 
   plugins: [
     react(),
     tailwindcss(),
