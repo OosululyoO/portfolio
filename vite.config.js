25171ae4befc403398f6fa169aec604017ba8a7e
OosululyoO <e940416@gmail.com>
2026-05-12 17:23:26 +0800

    chore: migrate to netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // 這裡非常重要！必須對應你的 GitHub Repo 名稱
-  base: '/portfolio/', 
   plugins: [
     react(),
     tailwindcss(),
